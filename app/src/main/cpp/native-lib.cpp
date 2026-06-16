d4503e9105f1285d0897b5dc039a9d62c70cc334
DaniZakaria63 <daniootsutsuki@gmail.com>
2026-06-16 20:35:05 +0700

    initialize

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,10 @@
+#include <jni.h>
+#include <string>
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_id_my_daniza_reelcraft_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
